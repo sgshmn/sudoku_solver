62d4039d3a03bb9a7be2aca43ad1736b738e690d
yyw <woong0997@gmail.com>
2024-11-16 01:24:33 +0900

    다람쥐 사랑해

--- sudoku.py
+++ sudoku.py
@@ -19,9 +19,56 @@ p = [[0,2,0,0,9,0,8,1,0],
 #   1) 바로 풀 수 있으면 다 풀기
 # test
 
-# for i in range(9):
-#     for j in range(9):
+# r = row
+r = []
+for i in range(9):
+     r.append({1,2,3,4,5,6,7,8,9})
 
+# c = column
+c = []
+for i in range(9):
+     c.append({1,2,3,4,5,6,7,8,9})
+
+# b = box
+# 00 01 02
+# 10 11 12
+# 20 21 22
+b = []
+for i in range(3):
+     bb = []
+     for j in range(3):
+          bb.append({1,2,3,4,5,6,7,8,9})
+     b.append(bb)
+
+
+for i in range(9):
+    for j in range(9):
+          x = p[i][j]
+          if x != 0:
+               r[i].discard(x)
+               c[j].discard(x)
+               b[i//3][j//3].discard(x)
+
+print("problem")
+for i in p:
+     print(i)
+print()
+
+print("row")
+for i in r:
+     print(i)
+print()
+
+print("column")
+for i in c:
+     print(i)
+print()
+
+print("box")
+for i in b:
+     for j in i:
+          print(j)
+print()
 
 #   2) 바로 풀 수 없으면 하나씩 대입
 #   3) 모순을 발견하면 가장 최근에 대입했던 곳으로 돌아가기(스택을 사용)
